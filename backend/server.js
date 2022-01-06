bce074298118d1f94056632820652638f87f5720
Adam Szep <szepadam5@gmail.com>
2022-01-06 19:58:04 +0100

    feat: backend working with filters
    backend can now provide filtered results

--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,48 @@
-import mysql2 from 'mysql2';
+import mysql2 from 'mysql2/promise';
 import express from 'express';
 import cors from 'cors';
 
-let matches;
 const app = express();
 const port = 3000;
 app.use(cors());
 app.use(express.json());
 
-let connection = mysql2.createConnection({
+let matches;
+
+let connectionJson = {
   host: 'localhost',
   user: 'root',
   password: 'root',
   database: 'sport_calendar'
-})
+}
 
-connection.connect((err) => {
-  if (err) throw err;
-  let sql = "SELECT * FROM match_view;"
-  connection.query(sql, (err, results) => {
-    if (err) throw err;
-    matches = results;
-  })
-});
+
 
 app.get('/', (req, res) => {
   res.status(200).end();
 });
 
-app.post('/getMatchForMonth', (req, res) => {
+app.post('/getMatchForMonth', async (req, res) => {
+  let connection = await mysql2.createConnection(connectionJson);
+  let sql = "SELECT * FROM match_view "
+  if ('filters' in req.body) {
+    sql += "WHERE ";
+    let conditions = []
+    if ('city' in req.body.filters) conditions.push("CITY='" + req.body.filters.city + "' ");
+    if ('sport' in req.body.filters) conditions.push("SPORT='" + req.body.filters.sport + "' ");
+    if ('team' in req.body.filters) conditions.push("HOME_TEAM='" + req.body.filters.team + "' OR AWAY_TEAM='" + req.body.filters.team + "'");
+    sql += conditions.join('AND ');
+  }
+  sql += ";";
+  matches = await connection.execute(sql);
   let responseArray = [];
   let currentDate = new Date(req.body.year, req.body.month, 1);
-  matches.forEach(element => {
+  matches[0].forEach(element => {
     if ((element.DATE.getMonth() == currentDate.getMonth()) && (element.DATE.getYear() == currentDate.getYear())) {
       responseArray.push(element);
     }
   })
+  await connection.end();
   res.status(200).send(responseArray);
 });
 
