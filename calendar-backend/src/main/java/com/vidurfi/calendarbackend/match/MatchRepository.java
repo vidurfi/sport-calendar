748488a83c7c45108df6b1e4ba44db82e64a5b81
Adam Szep <szepadam5@gmail.com>
2022-01-28 09:54:50 +0100

    style: add space

--- calendar-backend/src/main/java/com/vidurfi/calendarbackend/match/MatchRepository.java
+++ calendar-backend/src/main/java/com/vidurfi/calendarbackend/match/MatchRepository.java
@@ -2,5 +2,5 @@ package com.vidurfi.calendarbackend.match;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface MatchRepository extends CrudRepository<Match, Integer>{
+public interface MatchRepository extends CrudRepository<Match, Integer> {
 }
